0dbfbf7603993ccf8e5ae851521d1721cd31975b
bsc-com-ne-14-20 <bsc-com-ne-14-20@Unima.ac.mw>
2026-05-26 17:48:35 +0200

    Revert "Merge pull request #16 from bsc-com-ne-14-20/feature/middleware"
    This reverts commit ae64eed13928db440895171861db05d2871aeae6, reversing
    changes made to 867a1ee12e7c4c508db2cbae1322cf97348856f3.

--- dummy_site/django-blog-main/accounts/logout.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LogoutView
-from django.contrib import messages 
-
-class CustomLogoutView(LogoutView):
-    next_page = 'auth-login'
-    
-    def dispatch(self, request, *args, **kwargs):
-        messages.success(request, "You have been logged out successfully.")
-        return super().dispatch(request, *args, **kwargs)
