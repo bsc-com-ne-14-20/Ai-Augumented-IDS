8fe9f2566b6b7358e44b09fc94dfae8575e36f4b
Dennis Bakaya <bsc-32-22@unima.ac.mw>
2026-05-26 13:29:09 +0200

    changes in error reporting when registering

--- dummy_site/django-blog-main/accounts/forms.py
+++ dummy_site/django-blog-main/accounts/forms.py
@@ -2,6 +2,7 @@ import django.forms as forms
 from django.contrib.auth.models import User
 from .models import Profile
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,6 +10,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        
+        if password1 and password2:
+            if password1 != password2:
+                raise ValidationError("Passwords do not match.")
+        return password2
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
