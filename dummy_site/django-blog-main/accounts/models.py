568b38db41ed8e1101b580a8420a9c57b49e7dde
Dennis Bakaya <bsc-32-22@unima.ac.mw>
2026-05-26 15:39:26 +0200

    profile pic changes made

--- dummy_site/django-blog-main/accounts/models.py
+++ dummy_site/django-blog-main/accounts/models.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='profile_pics')
+    image = models.ImageField(upload_to='profile_pics', null=True, blank=True)
     
 
     def __str__(self):
