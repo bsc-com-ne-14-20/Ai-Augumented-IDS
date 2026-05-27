fd05794071afacc916654d8254494cba9797bcb6
Dennis Bakaya <167068199+BSC-32-22@users.noreply.github.com>
2026-05-28 00:24:09 +0200

    Update base URL for backend API services 2

--- Dashboard/aa_ids_dashboard/lib/constants.dart
+++ Dashboard/aa_ids_dashboard/lib/constants.dart
@@ -1,7 +1,7 @@
 
 class AppConstants {
   /// The base URL for the backend API services.
-  static const String baseUrl = 'https://d8c9-102-70-90-19.ngrok-free.app';
+  static const String baseUrl = 'https://oliva-uncomprehensive-marilu.ngrok-free.dev';
 
   
 }
